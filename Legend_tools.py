3f56a20f6dd819f265ae12713c14a8c12612c58b
08walkersj <zef014@uib.no>
2024-03-06 11:22:12 +0100

    restructuring repository

--- Legend_tools.py
+++
@@ -1,5 +0,0 @@
-def unique_legend(axis):
-    import numpy as np
-    handles, labels=axis.get_legend_handles_labels()
-    labels, ind= np.unique(labels, return_index=True)
-    return {'labels':list(labels), 'handles':list(np.array(handles)[ind])}
