351fd6effb7548b8563860553dc02628f241c4fb
simon_work <simon_james_walker@hotmail.co.uk>
2024-05-07 14:59:47 +0100

    correction of labels for box plot key

--- src/Plotting_Tools/Boxplot.py
+++ src/Plotting_Tools/Boxplot.py
@@ -12,19 +12,20 @@ def add_boxkey(axis, size=8, x_center=.175, scale= False, ypos= 9.5, width=.8, b
     boxkwargs.update(boxplotkwargs)
     txt_kwargs= {'size':size}
     txt_kwargs.update(text_kwargs)
-    print(boxkwargs)
     sample_boxplot = axis.boxplot(sample_data, **boxkwargs)
     text= [
         axis.text(sample_boxplot['medians'][0].get_xdata()[0], sample_boxplot['medians'][0].get_ydata()[0],
                 s='Median', ha='center', va='top', color=sample_boxplot['medians'][0].get_color(), **txt_kwargs),
-        axis.text(sample_boxplot['medians'][0].get_xdata()[0], sample_boxplot['medians'][0].get_ydata()[0]-y_offset*3,
-                s='Interquartile\nRange', ha='center', va='top', color=sample_boxplot['boxes'][0].get_color(), **txt_kwargs),
+        axis.text(sample_boxplot['boxes'][0].get_xdata()[0], sample_boxplot['boxes'][0].get_ydata()[0]-y_offset*3,
+                s='Lower\nQuartile', ha='center', va='top', color=sample_boxplot['boxes'][0].get_color(), **txt_kwargs),
+        axis.text(sample_boxplot['boxes'][0].get_xdata()[-2], sample_boxplot['boxes'][0].get_ydata()[-2]-y_offset*3,
+                s='Upper\nQuartile', ha='center', va='top', color=sample_boxplot['boxes'][0].get_color(), **txt_kwargs),
         axis.text(sample_boxplot['means'][0].get_xdata()[0], sample_boxplot['means'][0].get_ydata()[-1],
                 s='Mean', ha='center', va='bottom', color=sample_boxplot['means'][0].get_color(), **txt_kwargs),
         axis.text(sample_boxplot['caps'][0].get_xdata()[0], sample_boxplot['caps'][0].get_ydata()[-1],
-                s='Lower\nQuartile', ha='center', va='bottom', color=sample_boxplot['caps'][0].get_color(), **txt_kwargs),
+                s='Minimum', ha='center', va='bottom', color=sample_boxplot['caps'][0].get_color(), **txt_kwargs),
         axis.text(sample_boxplot['caps'][1].get_xdata()[0], sample_boxplot['caps'][1].get_ydata()[-1],
-                s='Upper\nQuartile', ha='center', va='bottom', color=sample_boxplot['caps'][1].get_color(), **txt_kwargs),
+                s='Maximum', ha='center', va='bottom', color=sample_boxplot['caps'][1].get_color(), **txt_kwargs),
         axis.text(sample_boxplot['fliers'][0].get_xdata()[0], sample_boxplot['fliers'][0].get_ydata()[-1]+y_offset,
                 s='Outliers', ha='center', va='bottom', color=sample_boxplot['fliers'][0].get_color(), **txt_kwargs)]
     return sample_boxplot, text
